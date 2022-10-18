fa69b13dab7a43f835512b500e39077229a08ca1
Vitaly Domnikov <dotcypress@gmail.com>
2022-10-18 09:23:51 -0700

    format code

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -8,24 +8,24 @@ pub use menu::*;
 pub use widgets::*;
 
 widget_mux! {
-  Viewport<&App>,
-  ViewportNode::MainMenu,
-  {
-      main_menu: MenuWidget;
-      config_menu: MenuWidget;
-      scan: ScanWidget;
-      send: SendWidget;
-      backlight: BacklightWidget;
-      sleep_timeout: SleepTimeoutWidget;
-      about: AboutWidget;
-  },
-  |widget: &mut Viewport, state: &App| {
-      widget.main_menu.update(&state.main_menu);
-      widget.config_menu.update(&state.config_menu);
-      widget.backlight.update(state.options.backlight);
-      widget.sleep_timeout.update(state.options.sleep_timeout);
-      widget.scan.update(state);
-      widget.send.update(state);
-      widget.set_active(state.active_widget);
-  }
+    Viewport<&App>,
+    ViewportNode::MainMenu,
+    {
+        main_menu: MenuWidget;
+        config_menu: MenuWidget;
+        scan: ScanWidget;
+        send: SendWidget;
+        backlight: BacklightWidget;
+        sleep_timeout: SleepTimeoutWidget;
+        about: AboutWidget;
+    },
+    |widget: &mut Viewport, state: &App| {
+        widget.main_menu.update(&state.main_menu);
+        widget.config_menu.update(&state.config_menu);
+        widget.backlight.update(state.options.backlight);
+        widget.sleep_timeout.update(state.options.sleep_timeout);
+        widget.scan.update(state);
+        widget.send.update(state);
+        widget.set_active(state.active_widget);
+    }
 }
